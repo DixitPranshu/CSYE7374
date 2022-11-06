1eed0e0c14f16b7ff7fc037981d74e2478d1a5c4
DixitPranshu <dixit.pr@northeastern.edu>
2022-11-06 01:42:20 -0500

    Optimized Java code for MerkleTree

--- src/main/java/crypto/MerkleTree.java
+++ src/main/java/crypto/MerkleTree.java
@@ -29,7 +29,7 @@ public class MerkleTree {
         ArrayList<TreeNode> childNodes = new ArrayList<>();
 
         for(HashMap.Entry<String, String> set: transactions.entrySet()){
-            childNodes.add(new TreeNode(null, null, generateHash(set)));
+            childNodes.add(new TreeNode(null, null, generateHash(set), set.getKey()));
         }
 //        for (String message : dataBlocks) {
 //            childNodes.add(new TreeNode(null, null, getHash(message)));
@@ -50,11 +50,11 @@ public class MerkleTree {
                 if ((index + 1) < length) {
                     rightChild = children.get(index + 1);
                 } else {
-                    rightChild = new TreeNode(null, null, leftChild.getHash());
+                    rightChild = new TreeNode(null, null, leftChild.getHash(), leftChild.getOriginalString());
                 }
                 HashMap.Entry<String,String> newSet = new AbstractMap.SimpleEntry<String, String>(leftChild.getHash() + rightChild.getHash(), "no");
                 String parentHash = generateHash(newSet);
-                parents.add(new TreeNode(leftChild, rightChild, parentHash));
+                parents.add(new TreeNode(leftChild, rightChild, parentHash, newSet.getKey()));
                 index += 2;
             }
             children = parents;
@@ -99,26 +99,48 @@ public class MerkleTree {
     }
 
     public static void main(String[] args) throws NoSuchAlgorithmException {
-        HashMap<String, String> transactions = new LinkedHashMap<>();
 
+        HashMap<String, String> transactions1 = new LinkedHashMap<>();
+        transactions1.put("00000dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", "no");
+        transactions1.put("Harry pays Robin 1.000","no");
+        transactions1.put("Maharshi pays Harry 1.000","no");
+        transactions1.put("Pranshu pays Maharshi 1.000","no");
+        TreeNode root1 = generateTree(transactions1);
+        String rootHash1 = root1.getHash();
 
         RandomStringGenerator generator = new RandomStringGenerator.Builder().withinRange('a', 'z').build();
 
-        String rootHash = "";
-        while(!rootHash.startsWith("0000")){
-            String randomLetters = generator.generate(10);
-            String nonce = randomLetters;
-            transactions.put("00000dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824", "no");
+
+        long total_program_run_time = 0;
+        for(int i=0;i<100;i++){
+            System.out.println("Run "+(i+1));
+            String rootHash = "";
+            long startTime = System.nanoTime();
+            while(!rootHash.startsWith("00000")){
+                HashMap<String, String> transactions2 = new LinkedHashMap<>();
+                HashMap<String, String> transactions3 = new LinkedHashMap<>();
+                String nonce = generator.generate(10);
 //            transactions.put(nonce,"no");
-            transactions.put("Harry pays Robin 1.000","no");
-            transactions.put("Maharshi pays Harry 1.000","no");
-            transactions.put("Pranshu pays Maharshi 1.000","no");
-            transactions.put("Robin pays Pranshu 1.000","no");
-            transactions.put(nonce,"no");
-            TreeNode root = generateTree(transactions);
-            rootHash = root.getHash();
-            System.out.println(root.getHash());
+
+                transactions2.put("Robin pays Pranshu 1.000","no");
+                transactions2.put(nonce,"no");
+                TreeNode root2 = generateTree(transactions2);
+                String rootHash2 = root2.getHash();
+
+                transactions3.put(rootHash1,"yes");
+                transactions3.put(rootHash2,"yes");
+                TreeNode root3 = generateTree(transactions3);
+                rootHash = root3.getHash();
+
+            }
+//            System.out.println(rootHash);
+            long endTime = System.nanoTime();
+            long totalTime = (endTime - startTime)/1000000000;
+            total_program_run_time+=totalTime;
+//            System.out.println("totalTime for mining: "+totalTime+" seconds");
         }
+        System.out.println("Avg totalTime for mining: "+total_program_run_time+" seconds");
+
 
 
 //        printLevelOrderTraversal(root);
--- src/main/java/crypto/TreeNode.java
+++ src/main/java/crypto/TreeNode.java
@@ -4,13 +4,15 @@ public class TreeNode {
 
         private TreeNode left;
         private TreeNode right;
+        private String originalString;
         private String hash;
         private String isHash;
 
-        public TreeNode(TreeNode left, TreeNode right, String hash) {
+        public TreeNode(TreeNode left, TreeNode right, String hash, String originalString) {
             this.left = left;
             this.right = right;
             this.hash = hash;
+            this.originalString = originalString;
         }
 
         public TreeNode getLeft() {
@@ -37,4 +39,11 @@ public class TreeNode {
             this.hash = hash;
         }
 
+        public String getOriginalString() {
+            return originalString;
+        }
+
+        public void setOriginalString(String originalString) {
+            this.originalString = originalString;
+        }
 }
